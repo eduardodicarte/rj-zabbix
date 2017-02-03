6e0daadb1b46a68be675313a5313a78a2adb5da7
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2017-02-03 00:45:39 -0200

    Configuração dos templates

--- manifests/centos/install/server.pp
+++ manifests/centos/install/server.pp
@@ -1,9 +1,23 @@
 class zabbix::centos::install::server {
+  $timezone = "America/Sao_Paulo"
+
   package {'zabbix-server-pgsql':
     ensure => present
   } ->
 
   package {'zabbix-web-pgsql':
     ensure => present
+  } ->
+
+  file {'zabbix.conf':
+    ensure  => file,
+    path    => '/etc/httpd/conf.d/zabbix.conf',
+    content => template("zabbix/zabbix.conf.erb")
+  } ->
+
+  file {'zabbix.conf.php':
+    ensure  => file,
+    path    => '/etc/zabbix/web/zabbix.conf.php',
+    content => template("zabbix/zabbix.conf.php.erb")
   }
 }
